04d7dfbdba64061f5185c5693369f18114e9953c
PPS <pavlina.psarsky@gmail.com>
2022-03-23 18:22:50 +0100

    game for desktop done

--- app.js
+++ app.js
@@ -1,24 +1,49 @@
+/* sound */
+const whackSound = new Audio('./sound/mixkit-metal-hammer-hit-833.wav');
+    whackSound.volume = 0.3;
+const gameOverSound = new Audio('./sound/mixkit-funny-game-over-2878.wav');
+    gameOverSound.volume = 0.4;
+const errorSound = new Audio('mixkit-game-show-buzz-in-3090.wav');
+    errorSound.volume = 1;
 
-    const squares = document.querySelectorAll('.square');
-    const mole = document.getElementsByClassName('mole');
-    const score = document.getElementById('score');
-    const timeLeft = document.getElementById('time-left');
-    const gameBoard = document.querySelector('.grid');
-    const gameOver = document.querySelector('.game-over');
-    const resultGameOver = document.getElementById('result');
-    const saysMartin = document.getElementById('saysMartin');
-    const newGameButton = document.getElementById('newGame');
+// sound toggle
+const soundButon = document.getElementById('soundOff');
+soundButon.addEventListener('click', () => {
+    let checkSound = soundButon.getAttribute('sound-off');
+    if(checkSound === 'false') {
+        soundButon.setAttribute('sound-off', true);
+        whackSound.volume = 0;
+        errorSound.volume = 0;
+        gameOverSound.volume = 0;
+    } else {
+        soundButon.setAttribute('sound-off', false);
+        whackSound.volume = 0.3;
+        errorSound.volume = 1;
+        gameOverSound.volume = 0.4;
+    }
+});
 
-    let lockboard = false;
+const squares = document.querySelectorAll('.square');
+const mole = document.getElementsByClassName('mole');
+const score = document.getElementById('score');
+const timeLeft = document.getElementById('time-left');
+const gameBoard = document.querySelector('.grid');
+const gameOver = document.querySelector('.game-over');
+const resultGameOver = document.getElementById('result');
+const saysMartin = document.getElementById('saysMartin');
+const newGameButton = document.getElementById('newGame');
+
+let lockboard = false;
     
 function playWhackAMole() {
     let result = 0;
     let hitPosition;
-    var currentTime = 5;
+    var currentTime = 30;
     var timerId = null;
     let martinsLine;
     
     if(lockboard === true) {
+        errorSound.play();
         return;
     }
 
@@ -39,6 +64,7 @@ function playWhackAMole() {
     squares.forEach(square => {
         square.addEventListener('mousedown', () => {
             if(square.id == hitPosition) {
+                whackSound.play();
                 result++;
                 score.innerText = result;
                 hitPosition = null;
@@ -47,7 +73,7 @@ function playWhackAMole() {
     })
 
     function moveMole() {
-        timerId = setInterval(selectRandom, 1000);
+        timerId = setInterval(selectRandom, 500);
     }
     moveMole()
 
@@ -65,6 +91,7 @@ function playWhackAMole() {
             resultGameOver.innerText = result;
             martinSays(result);
             saysMartin.innerText = martinsLine;
+            gameOverSound.play();
             lockboard = false;
         }
     }
@@ -72,16 +99,16 @@ function playWhackAMole() {
     function martinSays(score) {
         switch(true) {
             case score === 0:
-                martinsLine = "Martin is not happy. The mole will make more hills!"
+                martinsLine = "Martin is not happy.\n Moles will make more hills!"
                 break;
             case 0 < score && score < 6:
-                martinsLine = "Martin says, please, get more moles next time."
+                martinsLine = "Martin says, please,\n get more moles next time."
                 break;
             case 6 <= score && score <= 9:
-                martinsLine = "Martin is happy and says thank you!"
+                martinsLine = "Martin is happy\n and says thank you!"
                 break;
             case score >= 10:
-                martinsLine = "Martin is excited! No more hills!"
+                martinsLine = "Martin is excited!\n No more hills!"
                 break;
         }
     }
