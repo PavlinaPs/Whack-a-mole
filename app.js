aecb4afde58bb1ecb97b018da009086cd960a971
PPS <pavlina.psarsky@gmail.com>
2022-03-23 12:04:20 +0100

    switch with Martin's lines added

--- app.js
+++ app.js
@@ -2,11 +2,16 @@ const squares = document.querySelectorAll('.square');
 const mole = document.getElementsByClassName('mole');
 const score = document.getElementById('score');
 const timeLeft = document.getElementById('time-left');
+const gameBoard = document.querySelector('.grid');
+const gameOver = document.querySelector('.game-over');
+const resultGameOver = document.getElementById('result');
+const saysMartin = document.getElementById('saysMartin');
 
 let result = 0;
 let hitPosition;
-let currentTime = 10;
+let currentTime = 15;
 let timerId = null;
+let martinsLine;
 
 
 function selectRandom() {
@@ -44,7 +49,29 @@ function countDown() {
     if(currentTime == 0) {
         clearInterval(countDownTimerId);
         clearInterval(timerId);
-        alert('GAME OVER! Your final core is ' + result);
+        // alert('GAME OVER! Your final core is ' + result);
+        gameBoard.setAttribute('hidden', '');
+        gameOver.removeAttribute('hidden');
+        resultGameOver.innerText = result;
+        martinSays(result);
+        saysMartin.innerText = martinsLine;
+    }
+}
+
+function martinSays(score) {
+    switch(true) {
+        case score === 0:
+            martinsLine = "Martin is not happy. The mole will make more hills!"
+            break;
+        case 0 < score && score < 6:
+            martinsLine = "Martin says, please, get more moles next time."
+            break;
+        case 6 <= score && score <= 9:
+            martinsLine = "Martin is happy!"
+            break;
+        case score >= 10:
+            martinsLine = "Martin is excited! Moles have been eliminated!"
+            break;
     }
 }
 
